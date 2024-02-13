462d7354b5cb7f06a01664644e233511798bca73
rikharthulee <rikharthulee@gmail.com>
2024-02-13 18:58:59 +0000

    Created Array for Questions

---
+++ main.js
@@ -0,0 +1,51 @@
+const quizQuestions = [
+  {
+    Q1: "Which is the only country in the world to not have a non rectangualar flag?",
+    correctAnswer: "Nepal",
+  },
+
+  {
+    Q2: "What is the most abundant gas in our atmosphere?",
+    correctAnswer: "Nitrogen",
+  },
+
+  {
+    Q3: "When was the movie the Titanic released?",
+    correctAnswer: "1997",
+  },
+
+  {
+    Q4: "What is the name of the main antagonist in the Shakespeare play Othello?",
+    correctAnswer: "Lago",
+  },
+
+  {
+    Q5: "How many of Henry VIII's wives were called Catherine?",
+    correctAnswer: 3,
+  },
+
+  {
+    Q6: "What was the most popular girls name in the UK in 2021?",
+    correctAnswer: "Olivia",
+  },
+
+  {
+    Q7: "What is the capital of Finland?",
+    correctAnswer: "Helsinki",
+  },
+
+  {
+    Q8: "What is the currency of Denmark?",
+    correctAnswer: "Krone",
+  },
+
+  {
+    Q9: "What is the smallest planet in our solar system?",
+    correctAnswer: "Mercury",
+  },
+
+  {
+    Q10: "Street artist Banksy is originally associated with which British city?",
+    correctAnswer: "Bristol",
+  },
+];
