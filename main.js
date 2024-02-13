babf1340ce3330eca0f3699ef4c0c5a97521a28f
rikharthulee <rikharthulee@gmail.com>
2024-02-13 18:03:50 +0000

    Ticket 5 Complete

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ console.log(songs.length);
 console.log(songs[0]);
 
 songs[4] = "Baby Shark";
+
+//Iterate through the list and * cookies by 10
+
+let cookiesOrdered = [15, 3, 67, 1, 22, 100];
+
+for (let i = 0; i < cookiesOrdered.length; i++) {
+  cookiesOrdered[i] = cookiesOrdered[i] * 10;
+}
+
+console.log(cookiesOrdered);
